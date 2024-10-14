40c41170623d106e37cbbf49b89870ef958e3662
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2024-10-14 07:09:14 -0400

    added clarifying comments to dnd_base_stats_calculator.R

--- dnd_base_stats_calculator.R
+++ dnd_base_stats_calculator.R
@@ -1,10 +1,11 @@
-# dungeons and dragons: rolling character stats
+# dungeons and dragons: rolling character base stats.
 # a simple calculator by finity_cardinal in 'r'
 # i'm 99.9% certain a better version of this calculator exists. 
 # i still wanted to see if i could create this function from scratch...
 dnd_single_stat <- function(){
-  dnd_roll <- sample(1:6, 4, replace=TRUE) |> sort()
-  numbers_final <- dnd_roll[-1]
-  sum(numbers_final)
+  dnd_roll <- sample(1:6, 4, replace=TRUE) |> sort() # roll four six-sided dice, sort outputs in order
+  numbers_final <- dnd_roll[-1] # drop the lowest output 
+  sum(numbers_final) # sum up the remaining outputs 
 }
-dnd_stats <- c(replicate(6, dnd_single_stat()))
+dnd_stats <- c(replicate(6, dnd_single_stat())) # repeat this process six times 
+# now you have supplied a set of 6 base stats for your character. 
